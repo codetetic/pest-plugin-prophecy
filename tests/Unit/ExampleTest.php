22be04f342393da2b3ca2dcf94872615481a562d
Peter Measham <github@codetetic.co.uk>
2025-05-14 23:51:49 +0100

    add reveal to example test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,8 +1,11 @@
 <?php
 
-use function Pest\Prophecy\{prophesize};
+use function Pest\Prophecy\{prophesize, reveal};
 
 it('can be accessed as function', function (): void {
     expect(prophesize(DateTime::class))
         ->toBeInstanceOf(Prophecy\Prophecy\ObjectProphecy::class);
+
+    expect(reveal())
+        ->toBeInstanceOf(DateTime::class);
 });
