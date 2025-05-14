9fc436427eeb45f405e1b189cf8c0e4d550b1999
Peter Measham <github@codetetic.co.uk>
2025-05-14 23:08:35 +0100

    improve formatting and extend tests

--- tests/Unit/ArgumentTest.php
+++ tests/Unit/ArgumentTest.php
@@ -1,18 +1,24 @@
 <?php
 
-use function Pest\Prophecy\{prophesize, exact, type, which};
+use function Pest\Prophecy\{prophesize, exact, type, which, that, any};
 
-it('can be mocked with exact argument', function (): void {
+it('can be asserted with exact()', function (): void {
     $mock = prophesize(DateTime::class);
-    $mock->format(exact('format'))->willReturn('result');
+
+    $mock
+        ->format(exact('format'))
+        ->willReturn('result');
 
     expect($mock->reveal()->format('format'))
         ->toBe('result');
 });
 
-it('can be mocked with type argument', function (): void {
+it('can be asserted with type()', function (): void {
     $mock = prophesize(DateTime::class);
-    $mock->format(type('string'))->willReturn('result');
+
+    $mock
+        ->format(type('string'))
+        ->willReturn('result');
 
     expect($mock->reveal()->format('format'))
         ->toBe('result');
@@ -26,13 +32,42 @@ class ArgumentTest1
     }
 }
 
-it('can be mocked with which argument', function (): void {
+it('can be asserted with which()', function (): void {
     $mock = prophesize(ArgumentTest1::class);
-    $mock->test(which('format', 'string'))->willReturn('result');
+
+    $mock
+        ->test(which('format', 'string'))
+        ->willReturn('result');
 
     $object = new stdClass();
     $object->format = 'string';
 
     expect($mock->reveal()->test($object))
+        ->toBe('result');
+});
+
+it('can be asserted with that()', function (): void {
+    $mock = prophesize(DateTime::class);
+
+    $mock
+        ->format(
+            that(function(string $format): bool {
+                expect($format)->toBe('format');
+                return $format === 'format';
+            }))
+        ->willReturn('result');
+
+    expect($mock->reveal()->format('format'))
+        ->toBe('result');
+});
+
+it('can be asserted with any()', function (): void {
+    $mock = prophesize(DateTime::class);
+
+    $mock
+        ->format(any())
+        ->willReturn('result');
+
+    expect($mock->reveal()->format('format'))
         ->toBe('result');
 });
