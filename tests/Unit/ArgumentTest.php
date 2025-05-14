cbfe4fe896ed6b7bbb0e423eda2e945bdb3a61a2
Peter Measham <github@codetetic.co.uk>
2025-05-14 22:59:07 +0100

    extends tests

--- tests/Unit/ArgumentTest.php
+++ tests/Unit/ArgumentTest.php
@@ -1,6 +1,6 @@
 <?php
 
-use function Pest\Prophecy\{prophesize, exact, type};
+use function Pest\Prophecy\{prophesize, exact, type, which};
 
 it('can be mocked with exact argument', function (): void {
     $mock = prophesize(DateTime::class);
@@ -15,5 +15,24 @@ it('can be mocked with type argument', function (): void {
     $mock->format(type('string'))->willReturn('result');
 
     expect($mock->reveal()->format('format'))
+        ->toBe('result');
+});
+
+class ArgumentTest1
+{
+    public function test(stdClass $object): string
+    {
+        return 'test';
+    }
+}
+
+it('can be mocked with which argument', function (): void {
+    $mock = prophesize(ArgumentTest1::class);
+    $mock->test(which('format', 'string'))->willReturn('result');
+
+    $object = new stdClass();
+    $object->format = 'string';
+
+    expect($mock->reveal()->test($object))
         ->toBe('result');
 });
