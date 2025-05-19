b466e880031dd9b8bf109e22c7e171862f9eba7e
Peter Measham <github@codetetic.co.uk>
2025-05-19 19:39:02 +0100

    improve tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,10 +3,24 @@
 use function Pest\Prophecy\prophesize;
 use function Pest\Prophecy\reveal;
 
+class Example
+{
+}
+
 it('can be accessed as function', function (): void {
-    expect(prophesize(stdClass::class))
+    $prophecy = prophesize(Example::class);
+
+    expect($prophecy)
         ->toBeInstanceOf(Prophecy\Prophecy\ObjectProphecy::class);
 
-    expect(reveal(stdClass::class))
-        ->toBeInstanceOf(stdClass::class);
+    expect($prophecy->reveal())
+        ->toBeInstanceOf(Example::class);
+});
+
+it('can be accessed as function with reveal helper', function (): void {
+    expect(prophesize(Example::class))
+        ->toBeInstanceOf(Prophecy\Prophecy\ObjectProphecy::class);
+
+    expect(reveal(Example::class))
+        ->toBeInstanceOf(Example::class);
 });
