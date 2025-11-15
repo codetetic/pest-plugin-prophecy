cffbb18fc8390106a5b3a260c778ff9f9e96d64c
Peter Measham <github@codetetic.co.uk>
2025-11-15 20:08:35 +0000

    setup testing with devenv

---
+++ devenv.nix
@@ -0,0 +1,63 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  cachix.pull = [ "codetetic-php" ];
+
+  # https://devenv.sh/basics/
+
+  # https://devenv.sh/packages/
+  packages = [ ];
+
+  # https://devenv.sh/languages/
+  languages.php = {
+    enable = true;
+    version = "8.2";
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+  '';
+
+  # https://devenv.sh/tasks/
+  tasks = {
+    "app:setup" = {
+      exec = ''
+        composer install
+      '';
+      execIfModified = [
+        "composer.json"
+        "composer.lock"
+      ];
+    };
+    "devenv:enterShell".after = [ "app:setup" ];
+  };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    ./vendor/bin/pest --parallel
+  '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    trim-trailing-whitespace = {
+      enable = true;
+    };
+
+    pint = {
+      enable = true;
+      name = "Pint Code Style Fixer";
+      entry = "./vendor/bin/pint --test";
+      types = [ "php" ];
+    };
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
